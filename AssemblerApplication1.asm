069002cf5f7d135c6476027dce99306081f5fef5
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2025-03-25 14:14:34 -0300

    Update AssemblerApplication1.asm

--- AssemblerApplication1.asm
+++ AssemblerApplication1.asm
@@ -11,17 +11,18 @@ start:
 	clr r16 ; zera o registrador...usar uma das formas
 	
 	; DDRB = 0000 0000 -> dizer que PORT B é input
-	STS DDRB,r16 ; DDRB <-  00000000
+	OUT DDRB,r16 ; DDRB <-  00000000
 	
 	; PORT D é output DDRD = 1111 1111
 	SER r16; r16 <- 1111 1111
-	STS DDRD, r16
+	OUT DDRD, r16 ; grava no registrador
 
 	loop:
 	; ler a porta B
 	lds r16, PINB ; PINB são os pinos de entrada de PORT B
 	ldi r17, 15 ; r17 <- 15
+	; soma 15 em PORT B
 	add r16, r17 ; r16 <- r16 + r17
 
-	STS PORTD, r16 ; PORT D recebe a soma
+	OUT PORTD, r16 ; PORT D recebe a soma
 	rjmp loop ; volta
