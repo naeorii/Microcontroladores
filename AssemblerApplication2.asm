347340bd386cd74e42423dbece6d26134a378d55
Taiana Faleiro dos Santos <136939790+naeorii@users.noreply.github.com>
2025-03-27 14:54:57 -0300

    Update AssemblerApplication2.asm

--- AssemblerApplication2.asm
+++ AssemblerApplication2.asm
@@ -5,31 +5,40 @@
 ; Author : aluno
 ;
 
-.dseg 
+;
+; AssemblerApplication2.asm
+;
+; Created: 27/03/2025 13:59:14
+; Author : aluno
+;
+
+.dseg
 var:
- A: .byte 1;
- B: .byte 1;
-.cseg
+	A:	.byte 1;
+	B:  .byte 1;
+	.cseg
 
 start:
-;configurar portas de entrada e saída
-; x = in PORTD    y = out PORT B
-; x = x^2 + 2*x
-clr r16 ; r16 <- 0
-out DDRD, r16 ; PORTD é IN
-ldi r16, 0xFF ; r16 <- 255 ou 1111 1111
-out DDRB, r16 ; PORTB é out
+    ;Configurar portas e entrada
+	   ; =in PORTD y=out PORTB
+   	;y=x^2 + 2*x
 
-loop:
-in r16, PIND ; ler PORTD (A) e salvar em r16
-st A, r16 ; A  <- r16
-mov r17, r16 ; r17 <- r16 (A)
-add r17, r16 ; r17 <- r17 + r16 ou (2*x)
+	 clr r16			; r16 <- 0
+	 out DDRD, r16   ;PORTD é IN
+	 LDI r16, 0xFF	;r16 <- 255 ou 1111 1111
+	 out DDRB, r16	; PORTB é out
 
-mul r16, r16 ; r1:r0 <- r16 * r16 ou (A^2)
-add r17, r0 ; r17 + r0 ou 2*A + A^2
+	loop:
+		IN r16, PIND  ; ler portD (x) e salvar em r16
+		STS A, r16  ; var[0] recebe X
+		MOV r17, r16  ; r17 <- r16(A)
+		ADD r17, r16  ; r17 <- r17 + r16 (2*A)
 
-st B, r17 ; B <- r17
-out PORTB, r17 ; saída (PORTB) <- r17 
- 
-rjmp loop
+		MUL r16, r16  ;r1:r0 <- r16*r16
+		ADD r17, r0   ; r17 <- r17 + r0
+		sts b, r17
+
+		out PORTB, r17   ;saída <-r17
+
+
+		rjmp loop
