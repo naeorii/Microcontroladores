1acae429233a240e4507c2f84528041bfd26310d
Isabela Acosta <118640598+isabelaacr@users.noreply.github.com>
2025-03-27 14:36:44 -0300

    Update AssemblerApplication2.asm

--- AssemblerApplication2.asm
+++ AssemblerApplication2.asm
@@ -1,4 +1,4 @@
-
+;
 ; AssemblerApplication2.asm
 ;
 ; Created: 27/03/2025 14:21:13
@@ -7,7 +7,8 @@
 
 .dseg 
 var:
-x: .byte 2;
+ A: .byte 1;
+ B: .byte 1;
 .cseg
 
 start:
@@ -20,9 +21,15 @@ ldi r16, 0xFF ; r16 <- 255 ou 1111 1111
 out DDRB, r16 ; PORTB é out
 
 loop:
-in r16, PIND ; ler PORTD (x) e salvar em r16
-st var, r16 ; var[0] recebe x
-mov r17, r16 ; r17 <- r16 (x)
-add r17, r16 ; r17 <- r17 + r16 (2*x)
+in r16, PIND ; ler PORTD (A) e salvar em r16
+st A, r16 ; A  <- r16
+mov r17, r16 ; r17 <- r16 (A)
+add r17, r16 ; r17 <- r17 + r16 ou (2*x)
 
+mul r16, r16 ; r1:r0 <- r16 * r16 ou (A^2)
+add r17, r0 ; r17 + r0 ou 2*A + A^2
+
+st B, r17 ; B <- r17
+out PORTB, r17 ; saída (PORTB) <- r17 
+ 
 rjmp loop
